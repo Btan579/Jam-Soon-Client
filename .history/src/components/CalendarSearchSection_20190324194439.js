c34097be85a1e1e1e5c4e8fa3b194f4b1cded2d8
Brendan Tanner <btan579@gmail.com>
2019-03-27 12:31:20 -0400

    "commit before merge with master"

--- .history/src/components/CalendarSearchSection_20190324194439.js
+++ .history/src/components/CalendarSearchSection_20190324194439.js
@@ -7,7 +7,9 @@ export default class CalendarSearchSection extends React.Component {
     
         return (
             <div className="calendar-search-section">
-           
+            <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css">
+            <script src="//code.jquery.com/jquery-1.12.4.js"></script>
+            <script src="//code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
             <header>
                 <h3>Find some shows</h3>
             </header>
@@ -19,7 +21,7 @@ export default class CalendarSearchSection extends React.Component {
 
                 <button type='submit'>Submit</button>
             </form>
-         
+            <div className="datepicker"></div>
             <form className='calender-search-input'>
                 <div>
                     <label htmlFor = "date" > Date </label>
@@ -27,7 +29,9 @@ export default class CalendarSearchSection extends React.Component {
                     <p>[Placeholder for jQuery datepicker calender plugin component]</p>
                 </div>
             </form>
-     
+            <script>
+                $(".datepicker").datepicker(); 
+                </script>
             </div>
             
         );
